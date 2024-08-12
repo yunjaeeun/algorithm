cfc25490ade18b348ae3a87658b68085cf53f877
yunjaeeun <yunjaeeun12@naver.com>
2024-08-12 19:18:35 +0900

    24.08.12 공부내용 1. 369

--- swea/D2/D2.1926.py
+++ swea/D2/D2.1926.py
@@ -1,13 +1,27 @@
-num = int(input())
+# num = int(input())
+#
+# arr = ["3", "6", "9"]
+#
+# for i in range(1, num + 1):
+#     rep = 0
+#     for j in range(len(str(i))):
+#         if str(i)[j] in arr:
+#             rep += 1
+#     if rep != 0:
+#         print("-" * rep, end=" ")
+#     else:
+#         print(i, end=" ")
 
-arr = ["3", "6", "9"]
+N = int(input())
+clap = ["3", "6", "9"]
+for i in range(1, N + 1):
+    str_num = str(i)
+    count = 0
+    for char in str_num:
+        if char in clap:
+            count += 1
 
-for i in range(1, num + 1):
-    rep = 0
-    for j in range(len(str(i))):
-        if str(i)[j] in arr:
-            rep += 1
-    if rep != 0:
-        print("-" * rep, end=" ")
+    if count > 0:
+        print("-" * count, end=" ")
     else:
-        print(i, end=" ")
+        print(str_num, end=" ")
