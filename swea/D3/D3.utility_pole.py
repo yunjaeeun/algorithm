4cad71783538939b446735974a0343a996e800ca
yunjaeeun <yunjaeeun12@naver.com>
2024-08-29 15:48:53 +0900

    파일명 수정

--- swea/D3/D3.utility_pole.py
+++
@@ -1,7 +0,0 @@
-T = int(input())
-for tc in range(1, T + 1):
-    N = int(input())
-    arr = [list(map(int, input().split())) for _ in range(N)]
-
-    for i in range(N):
-        for j in range(N)
