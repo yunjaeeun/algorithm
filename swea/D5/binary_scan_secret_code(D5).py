84c1838d8f23214c39096f7720a65c5ac375505b
yunjaeeun <yunjaeeun12@naver.com>
2024-08-31 22:09:02 +0900

    24.08.31 공부내용 1. D5.복호화 풀이중

--- swea/D5/binary_scan_secret_code(D5).py
+++ swea/D5/binary_scan_secret_code(D5).py
@@ -38,7 +38,8 @@
 00000000000000000000000000000000000000000000000000
 
 '''
-
+import sys
+sys.stdin = open("input.txt", "r")
 
 def h_to_b(hex_value):
     result = ""
@@ -48,88 +49,7 @@ def h_to_b(hex_value):
     return result
 
 
-# def valid_code(code):
-#     code_len = len(code)
-#     code_ratio = []
-#     total_code = []
-#     total_cnt = 0
-#     for i in range(code_len):
-#         # 처음에 오는 0은 배제
-#         if code[i] == "1":
-#             part_len = 0
-#             cnt = 0
-#             add_cnt = 0
-#             current_code = code[i]
-#             current_idx = i
-#             # 찾은 위치부터 코드 탐색 시작
-#             start_idx = i
-#             while current_idx < code_len:
-#                 # 만약 같으면 길이 증가
-#                 if code[current_idx] == current_code:
-#                     cnt += 1
-#                     current_idx += 1
-#                 # 다른걸 만나면 비율 추가, 길이 초기화
-#                 else:
-#                     code_ratio.append(cnt)
-#                     add_cnt += 1
-#                     part_len += cnt
-#                     cnt = 0
-#                     current_code = code[current_idx]
-#                 # 비율 3개 저장했으면 일치하는지 확인
-#                 if add_cnt == 3 and part_len == 7:
-#                     rate_1, rate_2, rate_3 = code_ratio
-#                     while rate_1 > 0:
-#                         if rate_1 % 1 == 0 and rate_2 % 1 == 0 and rate_3 % 1 == 0:
-#                             now_ratio = ""
-#                             now_ratio += str(int(rate_1))
-#                             now_ratio += str(int(rate_2))
-#                             now_ratio += str(int(rate_3))
-#
-#                             # 일치하는 비율이 있다면
-#                             if now_ratio in decryption_dict:
-#                                 total_code.append(decryption_dict[now_ratio])
-#                                 total_cnt += 1
-#                                 code_ratio.clear()
-#                                 add_cnt = 0
-#                                 start_idx = current_idx
-#                                 part_len = 0
-#                                 break
-#                             # 일정한 비율을 확인하기 위해 2씩 나누기
-#                             rate_1 /= 2
-#                             rate_2 /= 2
-#                             rate_3 /= 2
-#                         # 비율이 존재하지 않으면
-#                         else:
-#                             add_cnt = 0
-#                             code_ratio.clear()
-#                             current_idx = start_idx + 1     # 다음 위치부터 탐색
-#                             part_len = 0
-#                             break
-#                 elif part_len != 7 and add_cnt == 3:
-#                     current_idx = current_idx - part_len
-#                     add_cnt = 0
-#                     part_len = 0
-#     return total_code
-
-def valid_code(code):
-    code_len = len(code)
-    # 앞의 0 배제
-    for i in range(code_len):
-        if code[i] == "1":
-            start_idx = i
-            break
-
-
-T = int(input())
-bi_dict = {"0": "0000", "1": "0001", "2": "0010", "3": "0011", "4": "0100", "5": "0101", "6": "0110", "7": "0111",
-           "8": "1000", "9": "1001", "A": "1010", "B": "1011", "C": "1100", "D": "1101", "E": "1110", "F": "1111"}
-decryption_dict = {"211": 0, "221": 1, "122": 2, "411": 3, "132": 4, "231": 5, "114": 6, "312": 7, "213": 8, "112": 9}
-
-for tc in range(1, T + 1):
-    N, M = map(int, input().split())
-
-    arr = [list(input()[:M + 1]) for _ in range(N)]
-
+def valid_start():
     for i in range(N):
         current_j = M - 1
         end = 0
@@ -147,7 +67,87 @@ for tc in range(1, T + 1):
                         break
 
                 h_value = arr[i][start + 1:end + 1]
-                b_value = h_to_b(h_value)
+                b_value = h_to_b(h_value)+"00000"
                 result = valid_code(b_value)
-                print(result)
+                test = 0
+                if result:
+                    test = is_code(result)
+                if test:
+                    return test
                 current_j = start - 1
+    return 0
+
+
+def valid_code(code):
+    secret_code = []
+    code_len = len(code)
+    current_idx = 0
+    code_ratio = []
+    while current_idx < code_len:  # 전체 탐색
+        if len(secret_code) == 8:
+            return secret_code
+        if code[current_idx] == "1":  # 비밀 코드의 시작은 항상 1부터 시작임
+            ctn = 0  # 코드가 차지하는 칸수
+            valid_idx = current_idx  # 탐색할 인덱스
+            current_code = code[current_idx]  # 현재 코드 1 or 0
+            while valid_idx < code_len and len(code_ratio) < 3:  # 앞의 0은 배제하므로 3개가 채워질 때 까지 반복
+                if current_code == code[valid_idx]:  # 이전것과 같다면
+                    ctn += 1  # 카운트 +1
+                    valid_idx += 1  # 탐색 인덱스 +1
+                elif current_code != code[valid_idx]:  # 다른걸 찾았다면
+                    code_ratio.append(ctn)  # 현재 체크한 칸수 추가
+                    current_code = code[valid_idx]  # 찾을 코드 변경
+                    ctn = 0  # 차지하는 칸수 초기화
+
+            if len(code_ratio) == 3:  # 비율이 3개가 되면 비율 비교
+                rate_1, rate_2, rate_3 = code_ratio
+                while rate_1 % 1 == 0 and rate_2 % 1 == 0 and rate_3 % 1 == 0:  # 소숫점이 없을때까지 반복
+                    raito = str(int(rate_1)) + str(int(rate_2)) + str(int(rate_3))
+
+                    if raito in decryption_dict:  # 복호화 표에 있다면
+                        secret_code.append(decryption_dict[raito])  # 암호 등록
+                        current_idx = valid_idx  # 다음 탐색범위 지정
+                        break
+
+                    rate_1 /= 2
+                    rate_2 /= 2
+                    rate_3 /= 2
+                else:  # break가 되지 않으면 실행
+                    current_idx += 1  # 다음 인덱스부터 탐색
+                    code_ratio.clear()
+                code_ratio.clear()
+        else:
+            current_idx += 1
+    if len(secret_code) == 8:
+        return secret_code
+    else:
+        return 0
+
+
+def is_code(code_arr):
+    odd_num = 0
+    even_num = 0
+    for i in range(8):
+        if i % 2 == 0:
+            odd_num += code_arr[i]
+        else:
+            even_num += code_arr[i]
+    if ((odd_num * 3) + even_num) % 10 == 0:
+        return odd_num + even_num
+    else:
+        return 0
+
+
+T = int(input())
+bi_dict = {"0": "0000", "1": "0001", "2": "0010", "3": "0011", "4": "0100", "5": "0101", "6": "0110", "7": "0111",
+           "8": "1000", "9": "1001", "A": "1010", "B": "1011", "C": "1100", "D": "1101", "E": "1110", "F": "1111"}
+decryption_dict = {"211": 0, "221": 1, "122": 2, "411": 3, "132": 4, "231": 5, "114": 6, "312": 7, "213": 8, "112": 9}
+
+for tc in range(1, T + 1):
+    N, M = map(int, input().split())
+
+    arr = [list(input()[:M + 1]) for _ in range(N)]
+
+    a = valid_start()
+
+    print(f"#{tc} {a}")
