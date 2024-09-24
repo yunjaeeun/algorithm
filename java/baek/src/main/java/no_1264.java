1e460066251eb1da04379bfc1e277db44d593c82
yunjaeeun <yunjaeeun12@naver.com>
2024-09-24 21:26:29 +0900

    24.09.24

---
+++ java/baek/src/main/java/no_1264.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class no_1264 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] arr = {'a', 'e', 'i', 'o', 'u'};
+
+        while (true) {
+            String input = sc.nextLine();
+            if(input.equals("#")) {
+                break;
+            }
+            String a = input.toLowerCase();
+            int result = 0;
+            for (int i = 0; i < a.length(); i++) {
+                for (int j = 0; j < 5; j++) {
+                    if (a.charAt(i) == arr[j]) {
+                        result++;
+                    }
+                }
+            }
+            System.out.println(result);
+        }
+    }
+}
