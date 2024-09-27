93a46e98c84c3cf08b608a4c60b63aac8f9980d7
yunjaeeun <yunjaeeun12@naver.com>
2024-09-27 22:16:54 +0900

    24.09.27

---
+++ java/baek/src/main/java/No1284.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class No1284 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String a = sc.nextLine();
+            if(a.equals("0")){
+                break;
+            }
+
+            int aLen = a.length();
+
+            int zero = 0;
+            int one = 0;
+            int other = 0;
+            for (int i = 0; i < aLen; i++) {
+                if (a.charAt(i) == '1') {
+                    one++;
+                } else if (a.charAt(i) == '0') {
+                    zero++;
+                } else {
+                    other++;
+                }
+            }
+            int result = (zero * 4) + (one * 2) + (other * 3) + (aLen + 1);
+            System.out.println(result);
+        }
+
+    }
+}
