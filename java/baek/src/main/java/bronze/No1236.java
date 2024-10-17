35efbb8a42a18f747e7b8a9e5d2890799bb46879
yunjaeeun <yunjaeeun12@naver.com>
2024-10-17 10:31:56 +0900

    24.10.17 성지키기

--- java/baek/src/main/java/bronze/No1236.java
+++ java/baek/src/main/java/bronze/No1236.java
@@ -1,48 +1,54 @@
 package bronze;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class No1236 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        char[][] arr = new char[N][M];
+        sc.nextLine();
 
-        int n = Integer.parseInt(st.nextToken()); // 성의 세로 크기
-        int m = Integer.parseInt(st.nextToken()); // 성의 가로 크기
-        int colSecurity = n; // 층 수 만큼 경비원 수 초기화
-        char flag = ' '; // 층별 X가 나오면 한번 저장하고 다시 저장 안 되도록 하기 위한 것
-        int[] rowStatus = new int[m]; // 열에 추가할 경비원 수 저장
+        for (int i = 0; i < N; i++) {
+            String dum = sc.nextLine();
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = dum.charAt(j);
+            }
+        }
+        int rCnt = 0;
+        int cCnt = 0;
 
-        Arrays.fill(rowStatus, 1); // rowStatus 1(경비가 배치된)로 초기화
-
-        for (int i = 0; i < n; i++) { // 세로 만큼 반복
-            String castle = br.readLine(); // 성 상태 입력
-            char[] colStatus = castle.toCharArray(); // 성 상태 char[]로 변환
-
-            for (int j = 0; j < m; j++) { // 가로 만큼 반복
-                // 열에 추가할 경비원 수 계산 코드
-                if (colStatus[j] == 'X') { // 해당 열에 경비원이 있으면 그 열은 추가할 경비원 없음
-                    rowStatus[j] = 0;
+        for (int i = 0; i < N; i++) {
+            boolean rIsCan = true;
+            for (int j = 0; j < M; j++) {
+                if (arr[i][j] == 'X') {
+                    rIsCan = false;
                 }
-                // 행에 추가할 경비원 수 계산 코드
-                if (flag == (char)i) { // 이미 X가 나왔을 시, colSecurity 감소 없이 진행
-                    continue;
-                } else if (colStatus[j] == 'X') { // X(경비원)이 있을 시, 추가할 경비원 수 감소
-                    colSecurity--;
-                    flag = (char) i;
-                }
-            } // 가로 반복 끝
-        } // 세로 반복 끝
-
-        int rowSecurity = 0; // 열 기준으로 배치할 경비원 수 저장 공간
-        for (int i=0; i<m; i++) { // 배열 길이만큼 반복해서 배치할 경비원 수 더함
-            rowSecurity += rowStatus[i];
+            }
+            if (rIsCan) {
+                rCnt++;
+            }
         }
 
-        System.out.println(Math.max(colSecurity, rowSecurity)); // 추가할 경비원의 최솟값 출력
+        for (int i = 0; i < M; i++) {
+            boolean cIsCan = true;
+            for (int j = 0; j < N; j++) {
+                if (arr[j][i] == 'X') {
+                    cIsCan = false;
+                }
+            }
+            if (cIsCan) {
+                cCnt++;
+            }
+        }
+        rCnt = rCnt;
+        cCnt = cCnt;
+        int result = Math.max(rCnt, cCnt);
+        System.out.println(result);
+
     }
 }
