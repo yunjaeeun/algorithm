44a8e234f577f8a84d980a3c90946c83c1db2edb
yunjaeeun <yunjaeeun12@naver.com>
2024-11-05 17:17:14 +0900

    24.11.04 1268 반장찾기 완료

--- java/baek/src/main/java/silver/no1268.java
+++ java/baek/src/main/java/silver/no1268.java
@@ -3,6 +3,8 @@ package silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
 public class no1268 {
     public static void main(String[] args) throws IOException {
@@ -10,42 +12,34 @@ public class no1268 {
 
         int N = Integer.parseInt(br.readLine());
 
-        int[] cnt = new int[N];
         int[][] board = new int[N][5];
+        int max = 0;
+        int result = 0;
 
         for (int i = 0; i < N; i++) {
             String[] s = br.readLine().split(" ");
             for (int j = 0; j < 5; j++) {
-                int idx = Integer.parseInt(s[j]);
-                board[i][j] = idx;
+                board[i][j] = Integer.parseInt(s[j]);
             }
         }
 
-        // 1. 세로 탐색(범위 한칸씩 줄여가며)
-        // 2. 오른쪽으로 한칸씩 가며 탐색
-        for (int k = 0; k < 5; k++) {
-            for (int i = 0; i < N; i++) {
-                int cls = board[i][k];
-                for (int j = i + 1; j < N; j++) {
-                    if (cls == board[j][k]) {
-                        cnt[i]++;
-                        cnt[j]++;
+        for (int i = 0; i < N; i++) {
+            Set<Integer> sameClass = new HashSet<>();
+
+            for (int j = 0; j < 5; j++) {
+                for (int k = 0; k < N; k++) {
+                    if (board[i][j] == board[k][j] && k != i) {
+                        sameClass.add(k);
                     }
                 }
             }
-        }
 
-        int max_cnt = 0;
-        int result = 0;
-        // 3. 마지막 최종 카운트 집계
-        for (int i = N - 1; i >= 0; i--) {
-//            System.out.print(cnt[i] + ", ");
-            if (cnt[i] > max_cnt) {
-                max_cnt = cnt[i];
-                result = i + 1;
+            if (max < sameClass.size()) {
+                max = sameClass.size();
+                result = i;
             }
         }
 
-        System.out.println(result);
+        System.out.println(result + 1);
     }
 }
