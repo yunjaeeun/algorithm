119844fe384d645e646e635f0fea334ca70948fd
yunjaeeun <yunjaeeun12@naver.com>
2024-11-14 15:13:30 +0900

    24.11.14 별찍기

---
+++ java/baek/src/main/java/bronze/no2441.java
@@ -0,0 +1,24 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class no2441 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = N; i > 0; i--) {
+            for (int j = i; j < N; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
