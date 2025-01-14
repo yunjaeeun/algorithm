702136ff90cc55d305a919ae996e3fc67576d72e
yunjaeeun <yunjaeeun12@naver.com>
2025-01-14 22:47:41 +0900

    25.01.14 백준

--- java/baek/src/main/java/bronze/no18228.java
+++ java/baek/src/main/java/bronze/no18228.java
@@ -3,28 +3,31 @@ package bronze;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class no18228 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine(), " ");
-        int res = 0;
-        Queue<Integer> pq = new PriorityQueue<>();
-        for (int i=0;i<n;i++) {
-            int cur = Integer.parseInt(st.nextToken());
-            if (cur == -1) {
-                res += pq.poll();
-                pq.clear();
-                continue;
-            }
-            pq.offer(cur);
+        int input = Integer.parseInt(br.readLine());
+        int[] arr = new int[input];
+        String[] sArr = br.readLine().split(" ");
+        for (int i = 0; i < input; i++) {
+            arr[i] = Integer.parseInt(sArr[i]);
         }
-        System.out.println(res+pq.poll());
+
+        int lMin = 200000000;
+        int rMin = 200000000;
+        boolean isPen = false;
+        for (int i = 0; i < input; i++) {
+            if (!isPen && lMin > arr[i] && arr[i] != -1) {
+                lMin = arr[i];
+            } else if (arr[i] == -1) {
+                isPen = true;
+            } else if (isPen && rMin > arr[i] && arr[i] != -1) {
+                rMin = arr[i];
+            }
+        }
+
+        System.out.println(rMin + lMin);
     }
 }
