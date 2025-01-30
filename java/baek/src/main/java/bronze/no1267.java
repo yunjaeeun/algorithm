264931ad789f732eb07c3c99ba0e669f33ce585a
yunjaeeun <yunjaeeun12@naver.com>
2025-01-30 21:46:16 +0900

    1267

--- java/baek/src/main/java/bronze/no1267.java
+++ java/baek/src/main/java/bronze/no1267.java
@@ -1,25 +1,41 @@
 package bronze;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class no1267 {
-    public static void main(String args[]) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int K;
-        int YoungSik = 0, MinSik = 0;
-        for (int i = 0; i < N; i++) {
-            K = sc.nextInt();
-            YoungSik += ((K / 30) + 1) * 10;
-            MinSik += ((K / 60) + 1) * 15;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        //두 요금을 가자 저장
+        int ym = 0;
+        int mm = 0;
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            int M = Integer.parseInt(st.nextToken());
+            //요금을 30과 60으로 나눴을때 결과에 맞춰 요금을 저장한다.
+            if((M % 30) >= 0) {
+                ym += ((M / 30) + 1) * 10;
+            }else {
+                ym += (M / 30) * 10;
+            }
+            if((M % 60) >= 0) {
+                mm += ((M / 60) + 1) * 15;
+            }else {
+                mm += (M / 60) * 15;
+            }
         }
-        if (YoungSik == MinSik) {
-            System.out.println("Y M " + YoungSik);
-        } else if (YoungSik < MinSik) {
-            System.out.println("Y " + YoungSik);
-        } else if (YoungSik > MinSik) {
-            System.out.println("M " + MinSik);
+        //두 요금을 비교해서 출력
+        if(ym < mm) {
+            System.out.println("Y " + ym);
+        }else if(mm < ym) {
+            System.out.println("M " + mm);
+        }else {
+            System.out.println("Y M " + ym);
         }
     }
 }
